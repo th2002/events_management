a6ed671dc7ac6f8db2d1f5e52bff917f9b7e8475
Tran Nguyen Dang Huy <trannguyendanhuy2904@gmail.com>
2024-04-03 01:02:41 +0700

    hehehehe

--- components/admin/Header/DarkModeSwitcher.tsx
+++ components/admin/Header/DarkModeSwitcher.tsx
@@ -1,3 +1,4 @@
+'use client';
 import useColorMode from '@/hooks/admin/useColorMode';
 
 const DarkModeSwitcher = () => {
@@ -6,7 +7,7 @@ const DarkModeSwitcher = () => {
   return (
     <li>
       <label
-        className={`h-7.5 relative m-0 block w-14 rounded-full ${
+        className={`relative m-0 block h-7.5 w-14 rounded-full ${
           colorMode === 'dark' ? 'bg-primary' : 'bg-stroke'
         }`}
       >
@@ -20,7 +21,7 @@ const DarkModeSwitcher = () => {
           className="dur absolute top-0 z-50 m-0 h-full w-full cursor-pointer opacity-0"
         />
         <span
-          className={`shadow-switcher absolute left-[3px] top-1/2 flex h-6 w-6 -translate-y-1/2 translate-x-0 items-center justify-center rounded-full bg-white duration-75 ease-linear ${
+          className={`absolute left-[3px] top-1/2 flex h-6 w-6 -translate-y-1/2 translate-x-0 items-center justify-center rounded-full bg-white shadow-switcher duration-75 ease-linear ${
             colorMode === 'dark' && '!right-[3px] !translate-x-full'
           }`}
         >
--- hooks/admin/useColorMode.tsx
+++ hooks/admin/useColorMode.tsx
@@ -1,19 +1,23 @@
-import { useEffect } from "react";
-import useLocalStorage from "./useLocalStorage";
+'use client';
+import { useEffect } from 'react';
+import useLocalStorage from './useLocalStorage';
 
 const useColorMode = () => {
-  const [colorMode, setColorMode] = useLocalStorage("color-theme", "light");
+  const [colorMode, setColorMode] = useLocalStorage('color-theme', 'light');
 
   useEffect(() => {
-    const className = "dark";
-    const bodyClass = window.document.body.classList;
+    const className = 'dark';
+    if (typeof window !== 'undefined') {
+      const bodyClass = window.document.body.classList;
 
-    colorMode === "dark"
-      ? bodyClass.add(className)
-      : bodyClass.remove(className);
+      colorMode === 'dark'
+        ? bodyClass.add(className)
+        : bodyClass.remove(className);
+    }
   }, [colorMode]);
 
   return [colorMode, setColorMode];
 };
 
 export default useColorMode;
+
